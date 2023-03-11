ecb1278b1a12d0e07ca6b8361c9303e99a56a2bd
hooks_28 <1r56955415918@qq.com>
2023-03-11 21:43:42 +0800

    feat(@memo28/utils): 给String，Number，Object添加log函数并且由用户开启生效

--- packages/utils/src/entry.ts
+++ packages/utils/src/entry.ts
@@ -1,14 +1,11 @@
 /*
  * @Author: 邱狮杰
  * @Date: 2023-01-30 10:39:18
- * @LastEditTime: 2023-03-10 17:17:15
+ * @LastEditTime: 2023-03-11 21:42:58
  * @Description:
- * @FilePath: /memo/packages/utils/src/index.ts
+ * @FilePath: /memo/packages/utils/src/entry.ts
  */
-import { enableLogAttribute } from './log'
-
-enableLogAttribute()
-
+export { enableLogAttribute } from './log'
 export { Injection } from './Injection'
 export { Errors } from './errors/core'
 export { AnomalousChain, panicProcessing } from './errors/anomalousChain'
--- packages/utils/src/log.ts
+++ packages/utils/src/log.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: 邱狮杰
  * @Date: 2023-03-10 16:45:57
- * @LastEditTime: 2023-03-10 17:16:17
+ * @LastEditTime: 2023-03-11 21:41:25
  * @Description:
  * @FilePath: /memo/packages/utils/src/log.ts
  */
@@ -11,18 +11,21 @@ import { str } from '@memo28/types'
 const Pointer = ' ===> '
 
 export function enableLogAttribute() {
-  String.prototype.log = function (this: String, mark?: str): void {
+  String.prototype.log = function (this: String, mark?: str): string {
     if (mark) console.log(`${mark}${Pointer}`, this)
     else console.log(this)
+    return this as string
   }
 
-  Number.prototype.log = function (this: String, mark?: str): void {
+  Number.prototype.log = function (this: Number, mark?: str): number {
     if (mark) console.log(`${mark}${Pointer}`, this)
     else console.log(this)
+    return this as number
   }
 
-  Object.prototype.log = function (this: String, mark?: str): void {
+  Object.prototype.log = function (this: Object, mark?: str): object {
     if (mark) console.log(`${mark}${Pointer}`, this)
     else console.log(this)
+    return this as object
   }
 }
