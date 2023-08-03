65e1b128d02f9b1e1be6cc4299422dfb956c378f
hooks_28 <1r56955415918@qq.com>
2023-08-03 15:59:44 +0800

    feat: add script:updateDep

--- packages/serviceHelper/src/quickCompletionCRUDAssistant/impl.ts
+++ packages/serviceHelper/src/quickCompletionCRUDAssistant/impl.ts
@@ -2,63 +2,68 @@
  * @Author: 邱狮杰
  * @Date: 2023-06-04 10:02:10
  * @LastEditTime: 2023-06-04 10:52:13
- * @Description: 
+ * @Description:
  * @FilePath: /memo/packages/serviceHelper/src/quickCompletionCRUDAssistant/impl.ts
  */
 export interface QuickCompletionCRUDAssistantResponse<Get = unknown, Delete = unknown, Create = unknown, Update = unknown, Excel = unknown> {
-    get: Get
-    delete: Delete
-    create: Create
-    update: Update
-    excel: Excel
+  get: Promise<Get>;
+  delete: Promise<Delete>;
+  create: Promise<Create>;
+  update: Promise<Update>;
+  excel: Promise<Excel>;
 }
 
 
 /**
  * 配置路由后缀
- * 
- * 
+ *
+ *
  * @public
  */
 export interface ConfigureSuffix {
-    get: string
-    delete: string
-    create: string
-    update: string
-    excel: string
+  get: string;
+  delete: string;
+  create: string;
+  update: string;
+  excel: string;
 }
 
 export interface QuickCompletionCRUDAssistantConfig {
-    triggerFn: Function
-    url: string,
-    configureSuffix: ConfigureSuffix
+  triggerFn: Function
+  url: string,
+  configureSuffix: ConfigureSuffix
 }
 
 
 export interface TriggerResult<Req extends object, Res extends QuickCompletionCRUDAssistantResponse = QuickCompletionCRUDAssistantResponse> {
-    get(req: Req): Res['get']
-    del(req: Req): Res['delete']
-    create(req: Req): Res['create']
-    update(req: Req): Res['update']
-    excel(req: Req): Res['excel']
+  get(req: Req): Promise<Res["get"]>;
+
+  del(req: Req): Promise<Res["delete"]>;
+
+  create(req: Req): Promise<Res["create"]>;
+
+  update(req: Req): Promise<Res["update"]>;
+
+  excel(req: Req): Promise<Res["excel"]>;
 }
 
 export type customTrigger<Req extends object, Res extends QuickCompletionCRUDAssistantResponse = QuickCompletionCRUDAssistantResponse> = (fn: Function, url: string, TriggerResult: TriggerResult<Req, Res>) => object
 
 
-
 export abstract class QuickCompletionCRUDAssistantImpl<Req extends object, Res extends QuickCompletionCRUDAssistantResponse = QuickCompletionCRUDAssistantResponse> {
-    abstract setTriggeredRequestFunction(fn: Function): this
-    /**
-     * 
-     * 配置 路由的后缀
-     * 
-     * @example
-     * 
-     * @public
-     */
-    abstract setConfigureSuffix(config: Partial<ConfigureSuffix>): this
-    abstract setURL(url: string): this
+  abstract setTriggeredRequestFunction(fn: Function): this
 
-    abstract trigger(config?: Partial<customTrigger<Req, Res>>): TriggerResult<Req, Res>
+  /**
+   *
+   * 配置 路由的后缀
+   *
+   * @example
+   *
+   * @public
+   */
+  abstract setConfigureSuffix(config: Partial<ConfigureSuffix>): this
+
+  abstract setURL(url: string): this
+
+  abstract trigger(config?: Partial<customTrigger<Req, Res>>): TriggerResult<Req, Res>
 }
--- packages/serviceHelper/src/quickCompletionCRUDAssistant/index.ts
+++ packages/serviceHelper/src/quickCompletionCRUDAssistant/index.ts
@@ -1,6 +1,11 @@
-
-
-import { ConfigureSuffix, QuickCompletionCRUDAssistantConfig, QuickCompletionCRUDAssistantImpl, QuickCompletionCRUDAssistantResponse, TriggerResult, customTrigger } from './impl'
+import {
+  ConfigureSuffix,
+  QuickCompletionCRUDAssistantConfig,
+  QuickCompletionCRUDAssistantImpl,
+  QuickCompletionCRUDAssistantResponse,
+  TriggerResult,
+  customTrigger
+} from "./impl";
 
 /**
  *
@@ -41,71 +46,84 @@ import { ConfigureSuffix, QuickCompletionCRUDAssistantConfig, QuickCompletionCRU
  */
 export class QuickCompletionCRUDAssistant<Req extends object, T extends QuickCompletionCRUDAssistantResponse = QuickCompletionCRUDAssistantResponse> implements QuickCompletionCRUDAssistantImpl<Req, T> {
 
-    constructor(url?: string, configureSuffix?: Partial<ConfigureSuffix>) {
-        url && this.setURL(url)
-        configureSuffix && this.setConfigureSuffix(configureSuffix)
+  constructor(url?: string, configureSuffix?: Partial<ConfigureSuffix>) {
+    url && this.setURL(url);
+    configureSuffix && this.setConfigureSuffix(configureSuffix);
+  }
+
+
+  private config: QuickCompletionCRUDAssistantConfig = {
+    triggerFn: () => {
+    },
+    url: "",
+    configureSuffix: {
+      get: "get",
+      delete: "delete",
+      create: "create",
+      update: "update",
+      excel: "excel"
     }
 
+  };
 
-    private config: QuickCompletionCRUDAssistantConfig = {
-        triggerFn: () => { },
-        url: '',
-        configureSuffix: {
-            get: 'get',
-            delete: 'delete',
-            create: 'create',
-            update: 'update',
-            excel: 'excel'
-        }
+  setURL(url: string): this {
+    this.config.url = url;
+    return this;
+  }
 
-    }
+  setTriggeredRequestFunction(fn: Function): this {
+    this.config.triggerFn = fn;
+    return this;
+  }
 
-    setURL(url: string): this {
-        this.config.url = url
-        return this
-    }
-
-    setTriggeredRequestFunction(fn: Function): this {
-        this.config.triggerFn = fn
-        return this
-    }
-
-    setConfigureSuffix(config: Partial<ConfigureSuffix>) {
-        this.config.configureSuffix = {
-            ...this.config.configureSuffix,
-            ...config
-        }
-        return this
-    }
+  setConfigureSuffix(config: Partial<ConfigureSuffix>) {
+    this.config.configureSuffix = {
+      ...this.config.configureSuffix,
+      ...config
+    };
+    return this;
+  }
 
 
-    trigger<Tr extends customTrigger<Req, T>>(config?: Tr): TriggerResult<Req, T> & ReturnType<Tr> {
-        const basicTriggerCollection = {
-            get: (req: Req): T['get'] => {
-                return this.config.triggerFn({ url: this.config.url + this.config.configureSuffix.get, method: "GET", ...req })
-            },
-            del: (req: Req): T['delete'] => {
-                return this.config.triggerFn({ url: this.config.url + this.config.configureSuffix.delete, method: "DELETE", ...req })
-            },
-            create: (req: Req): T['create'] => {
-                return this.config.triggerFn({ url: this.config.url + this.config.configureSuffix.create, method: "POST", ...req })
-            },
-            update: (req: Req): T['update'] => {
-                return this.config.triggerFn({ url: this.config.url + this.config.configureSuffix.update, method: "PUT", ...req })
-            },
+  trigger<Tr extends customTrigger<Req, T>>(config?: Tr): TriggerResult<Req, T> & ReturnType<Tr> {
+    const basicTriggerCollection = {
+      get: async (req: Req): Promise<T["get"]> => {
+        return await this.config.triggerFn({
+          url: this.config.url + this.config.configureSuffix.get,
+          method: "GET", ...req
+        });
+      },
+      del: async (req: Req): Promise<T["delete"]> => {
+        return await this.config.triggerFn({
+          url: this.config.url + this.config.configureSuffix.delete,
+          method: "DELETE", ...req
+        });
+      },
+      create: async (req: Req): Promise<T["create"]> => {
+        return this.config.triggerFn({
+          url: this.config.url + this.config.configureSuffix.create,
+          method: "POST", ...req
+        });
+      },
+      update: async (req: Req): Promise<T["update"]> => {
+        return this.config.triggerFn({
+          url: this.config.url + this.config.configureSuffix.update,
+          method: "PUT", ...req
+        });
+      },
 
-            excel: (req: Req): T['excel'] => {
-                return this.config.triggerFn({ url: this.config.url + this.config.configureSuffix.excel, ...req })
-            },
-        }
+      excel: async (req: Req): Promise<T["excel"]> => {
+        return await this.config.triggerFn({ url: this.config.url + this.config.configureSuffix.excel, ...req });
+      }
+    };
 
-        const triggerList: ReturnType<Tr> = (config?.(this.config.triggerFn, this.config.url, basicTriggerCollection) || {}) as ReturnType<Tr>
+    const triggerList: ReturnType<Tr> = (config?.(this.config.triggerFn, this.config.url, basicTriggerCollection) || {}) as ReturnType<Tr>;
 
-        return {
-            ...basicTriggerCollection,
-            ...triggerList
-        }
-    }
+    return {
+      ...basicTriggerCollection,
+      ...triggerList
+    };
+  }
 }
 
 
@@ -130,9 +148,9 @@ export class QuickCompletionCRUDAssistant<Req extends object, T extends QuickCom
  * @public
  */
 export function unifyQuickCompletionCRUDAssistant<Req extends object>(configureSuffix: Partial<ConfigureSuffix>, triggerFn: Function) {
-    return <Res extends QuickCompletionCRUDAssistantResponse = QuickCompletionCRUDAssistantResponse>() => {
-        return new QuickCompletionCRUDAssistant<Req, Res>().setTriggeredRequestFunction(triggerFn).setConfigureSuffix(configureSuffix)
-    }
+  return <Res extends QuickCompletionCRUDAssistantResponse = QuickCompletionCRUDAssistantResponse>() => {
+    return new QuickCompletionCRUDAssistant<Req, Res>().setTriggeredRequestFunction(triggerFn).setConfigureSuffix(configureSuffix);
+  };
 }
 
 
