b7eb28d648e3bbf446517a220d80a81239b6615c
memo.repo <1r56955415918@qq.com>
2023-12-27 10:40:48 +0800

    :bug:fix(all): 本地修复github action build 报错问题

--- packages/rollup/vitest.config.js
+++ packages/rollup/vitest.config.js
@@ -1,10 +1,10 @@
 "use strict";
 /*
  * @Author: @memo28.repo
- * @Date: 2023-12-19 17:10:35
- * @LastEditTime: 2023-12-19 17:12:17
+ * @Date: 2023-12-19 17:10:41
+ * @LastEditTime: 2023-12-19 17:12:08
  * @Description:
- * @FilePath: /memo/packages/nullSafety/vitest.config.ts
+ * @FilePath: /memo/packages/rollup/vitest.config.ts
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const config_1 = require("vitest/config");
